41205351f415f1a17dbbf3bb25b93c6a3ff71f2d
pachecosamuel <pacheco.samuel@hotmail.com>
2026-01-26 20:27:47 -0300

    fundamento implementado

---
+++ apis/open_ai.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    base_url=os.getenv("BASE_URL"),
+    api_key=os.getenv("API_KEY")
+)
+
+response = client.chat.completions.create(
+    model="meta-llama-3.1-8b-instruct",
+    messages=[
+        {"role": "system", "content": "Você é um especialista em IA"},
+        {"role": "user", "content": "Explique o que é LangChain em uma frase"}
+    ]
+)
+
+print(response.choices[0].message.content)
